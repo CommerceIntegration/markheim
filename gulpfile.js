0b5978ae57794447d2d8df2c16be8210c10ad021
Mark Birbeck <mark.birbeck@gmail.com>
2015-03-13 16:44:41 +0000

    refactor: move globals processing to front matter module

--- gulpfile.js
+++ gulpfile.js
@@ -184,72 +184,7 @@ gulp.task('clean', function(cb) {
 
 var convert = lazypipe()
   .pipe(function() {
-    return es.map(function(file, cb) {
-      var fm = file.frontMatter;
-      gutil.log('front matter found in', file.relative, '=>', fm);
-
-      /**
-       * Some front matter properties need merging:
-       */
-
-      config.front_matter.merge.map(function(variables) {
-        if (!Array.isArray(fm[variables[0]])) {
-          fm[variables[0]] = [fm[variables[0]]];
-        }
-        if (!Array.isArray(fm[variables[1]])) {
-          fm[variables[1]] = [fm[variables[1]]];
-        }
-        fm[variables[0]] = merge(fm[variables[0]], fm[variables[1]]);
-        delete fm[variables[1]];
-      });
-
-
-      /**
-       * 'internals' variables are values for the SSG processor that don't get
-       * pushed through to templates. These values can be set in config and
-       * overridden in front matter:
-       */
-
-      var internals = file[config.ssg] = {};
-
-      config.front_matter.internals.map(function(variable) {
-        internals[variable] = fm[variable] || config[variable];
-        delete fm[variable];
-      });
-
-      /**
-       * 'page' variables are those for the page being processed and can only
-       * come from front matter. Note that we remove the variables we know about
-       * so that anything left is a custom variable:
-       */
-
-      var page = {};
-
-      config.front_matter.page.map(function(variable) {
-        page[variable] = fm[variable];
-        delete fm[variable];
-      });
-
-      /**
-       * Merge in custom variable:
-       */
-
-      page = merge(page, fm);
-
-      /**
-       * Finally set 'globals' to be an amalgam of 'site', 'page', 'content' and 'paginator':
-       */
-
-      file.globals = {
-        site: config,
-        page: page,
-        content: null,
-        paginator: null
-      };
-
-      gutil.log('  =>', file.globals.page);
-      cb(null, file);
-    });
+    return frontMatter.globals(config);
   });
 
 gulp.task('build', ['clean'], function() {
